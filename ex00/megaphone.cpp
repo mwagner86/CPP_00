c97c3ac018a9f551bb6b644f6a945329edd8ab58
mwagner <mwagner@students.42wolfsburg.de>
2023-06-21 12:28:59 +0200

    finalized ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -17,13 +17,11 @@ int main(int argc, char **argv)
 {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else {
-		while (*(++argv))
-			while (**argv)
-			{
-				std::cout << (char)std::toupper(**argv);
-				(*argv)++;
-			}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char) toupper(argv[i][j]);
 	}
 	std::cout << std::endl;
 }
