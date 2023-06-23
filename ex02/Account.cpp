8eb2efa1b4eb80446b22465ed847eef38d5e0638
mwagner <mwagner@students.42wolfsburg.de>
2023-06-23 15:01:37 +0200

    ex02 formatting fix and makefile comfort

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -12,6 +12,7 @@
 
 #include "Account.hpp"
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 
 int	Account::_nbAccounts = 0;
@@ -132,9 +133,10 @@ void	Account::_displayTimestamp( void ) {
 	std::time_t current_time = std::time(0);
 	std::tm* time_info = std::localtime(&current_time);
 
-	std::cout 	<< "[" << time_info->tm_year + 1900
-				 << time_info->tm_mon <<  time_info->tm_mday
-				 <<	'_' << time_info->tm_hour
-				 << time_info->tm_min <<
-				 time_info->tm_sec << "] ";
+	std::cout << "[" << time_info->tm_year + 1900
+			  << std::setfill('0') << std::setw(2) << time_info->tm_mon + 1
+			  << std::setfill('0') << std::setw(2) << time_info->tm_mday
+			  << '_' << std::setw(2) << time_info->tm_hour
+			  << std::setw(2) << time_info->tm_min
+			  << std::setw(2) << time_info->tm_sec << "] ";
 }
